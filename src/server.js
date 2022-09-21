474af545bbffb386ac99a4b33c9d41147da97078
jeffondev <jeffondev@gmail.com>
2022-09-21 22:10:31 +0900

    채팅룸

--- src/server.js
+++ src/server.js
@@ -1,25 +1,32 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from 'express';
+import { disconnect } from "process";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res)=> res.render("home"));
-app.get("/*", (req,res)=> res.redirect("/"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done) => {
-        console.log(roomName);
-        setTimeout(() => {
-            done();
-        },5000);
+        done();
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome")
     });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    })
 });
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
