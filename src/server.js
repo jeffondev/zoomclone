4992e128dfe4b620f59f1fa6fdf754be1de5d45c
jeffondev <jeffondev@gmail.com>
2022-09-21 18:13:08 +0900

    soket.io

--- src/server.js
+++ src/server.js
@@ -1,30 +1,26 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from 'express';
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res)=> res.render("home"));
 app.get("/*", (req,res)=> res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-wss.on("connection", (socket)=> {
-    sockets.push(socket);
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from Browser"));
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(`${message}`));
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (roomName, done) => {
+        console.log(roomName);
+        setTimeout(() => {
+            done();
+        },5000);
     });
-})
+});
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
